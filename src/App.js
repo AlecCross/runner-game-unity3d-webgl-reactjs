519c40a6c6a7436c490f1c525c1fdeae7c333bd9
AlecCross <cross2472@gmail.com>
2023-01-02 14:22:37 +0200

    setup progect - game is run!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 // import Unity from "react-unity-webgl"
-import Unity, {UnityContext} from "react-unity-webgl"
-
-const unityContext = new UnityContext({
-    loaderUrl: "./Game/Build/Build.loader.js",
-    dataUrl: "./Game/build/Build.data",
-    frameworkUrl: "./Game/build/Build.framework.js",
-    codeUrl: "./Game/build/Build.wasm",
-})
+import { Unity, useUnityContext } from "react-unity-webgl";
+import urlHome from "../package.json";
 
 function App() {
-  return <div style={{textAlign: "center"}}>
-    <h1>Runner-Game-Unity3d-WebGL-ReactJS</h1>
-    <Unity
-        style={{
-          width: 600,
-          height: 400,
-          border: "2px solid black",
-          backround: "green"
-        }}
-        unityContext={unityContext}
-    />
-  </div>
+  const { unityProvider } = new useUnityContext({
+    dataUrl: `${urlHome.homepage}build/ReactBuild.data`,
+    frameworkUrl: `${urlHome.homepage}build/ReactBuild.framework.js`,
+    loaderUrl: `${urlHome.homepage}build/ReactBuild.loader.js`,
+    codeUrl: `${urlHome.homepage}build/ReactBuild.wasm`,
+  });
+  return <Unity unityProvider={unityProvider} style={{ width: 800, height: 600, border: "solid black 1px" }} />;
 }
 
 export default App
